cfdb973bf55a5f604d9d509790bc4e6fd14ad6b4
Thiago Antenor <thiagoantenor31@gmail.com>
2024-05-07 23:09:08 -0300

    Commit fix: Refatoração do código do exercicio 2

--- src/controller/SteamController.java
+++ src/controller/SteamController.java
@@ -20,9 +20,9 @@ public class SteamController {
 		}
 	}
 
-	private String tradutorDeMes(int mes) {
+	private String conversorDeMes(int mes) {
 		String mesEmIngles = null;
-		switch(mes) {
+		switch (mes) {
 		case 1:
 			mesEmIngles = "January";
 			break;
@@ -64,12 +64,12 @@ public class SteamController {
 		}
 		return mesEmIngles;
 	}
-	
+
 	public void exibeMediaJogadores(int ano, int mes, double mediaEsperada) throws IOException {
 		excecaoAno(ano);
-		//mes vai vir em portugues e precisa ser traduzido em ingles
-		String mesEmIngles = tradutorDeMes(mes);
-		
+		// mes vai vir em inteiro e precisa ser convertido em string e estar em ingles
+		String mesEmIngles = conversorDeMes(mes);
+
 		File arq = new File("C:\\TEMP", "SteamCharts.csv");
 		FileInputStream fluxo = new FileInputStream(arq);
 		InputStreamReader leitor = new InputStreamReader(fluxo);
@@ -79,17 +79,18 @@ public class SteamController {
 		int coluna1;
 		double coluna3;
 		boolean achou = false;
-		
+
 		// primeira linha contem apenas os nomes das colunas
 		linha = buffer.readLine();
+
+		System.out.println("Nome do jogo e média de jogadores ativos: ");
 		
 		while (linha != null) {
 			String[] colunas = linha.split(",");
 			coluna1 = Integer.parseInt(colunas[1]);
 			coluna3 = Double.parseDouble(colunas[3]);
-			if (ano == coluna1 && mesEmIngles.equals(colunas[2])  && mediaEsperada >= coluna3) {
+			if (ano == coluna1 && mesEmIngles.equals(colunas[2]) && coluna3 >= mediaEsperada) {
 				achou = true;
-				System.out.print("Nome do jogo e média de jogadores ativos: ");
 				System.out.println(colunas[0] + " | " + coluna3);
 			}
 			linha = buffer.readLine();
@@ -99,38 +100,36 @@ public class SteamController {
 		buffer.close();
 		String fimLista;
 		if (achou) {
-			fimLista = "A lista de jogos chegou ao fim";			
-		}
-		else {
+			fimLista = "A lista de jogos chegou ao fim";
+		} else {
 			fimLista = "Nenhum jogo em " + mesEmIngles + ", " + ano
-					+ ", que tenha média de jogadores igual ou menor que: " + mediaEsperada;			
+					+ ", que tenha média de jogadores igual ou menor que: " + mediaEsperada;
 		}
 		System.out.println(fimLista);
 	}
-	
 
 	public void geraArquivoMediaDeJogadores(int ano, int mes, String caminho, String nomeArq) throws IOException {
 		excecaoAno(ano);
-		//mes vai vir em portugues e precisa ser traduzido em ingles
-		String mesEmIngles = tradutorDeMes(mes);
+		// mes vai vir em inteiro e precisa ser convertido em string e estar em ingles
+		String mesEmIngles = conversorDeMes(mes);
 
 		File dir = new File(caminho);
-		File dados = new File("C:\\TEMP", "SteamCharts.csv");
-		File arqNovo;
-		if (nomeArq.contains(".csv")) {
-			arqNovo = new File(caminho, nomeArq);
-		} else {
-			arqNovo = new File(caminho, nomeArq + ".csv");
-		}
-
 		if (dir.exists() && dir.isDirectory()) {
+			File dados = new File("C:\\TEMP", "SteamCharts.csv");
+			File arqNovo;
+			if (nomeArq.contains(".csv")) {
+				arqNovo = new File(caminho, nomeArq);
+			} else {
+				arqNovo = new File(caminho, nomeArq + ".csv");
+			}
+
 			FileInputStream fluxo = new FileInputStream(dados);
 			InputStreamReader leitor = new InputStreamReader(fluxo);
 			BufferedReader buffer = new BufferedReader(leitor);
 			FileWriter fileWriter = new FileWriter(arqNovo);
 			PrintWriter print = new PrintWriter(fileWriter);
 			String linha = buffer.readLine();
-			
+
 			// primeira linha contem apenas os nomes das colunas
 			linha = buffer.readLine();
 
@@ -139,7 +138,7 @@ public class SteamController {
 				String[] colunas = linha.split(",");
 				int coluna1 = Integer.parseInt(colunas[1]);
 				double coluna3 = Double.parseDouble(colunas[3]);
-				
+
 				if (ano == coluna1 && mesEmIngles.equals(colunas[2])) {
 					saida.append(colunas[0] + ";");
 					saida.append(coluna3 + "\r\n");
@@ -147,14 +146,15 @@ public class SteamController {
 				}
 				linha = buffer.readLine();
 			}
-			buffer.close();
+			
 			print.flush();
+			buffer.close();
 			fluxo.close();
 			leitor.close();
 			fileWriter.close();
 			print.close();
-			
-			if (arqNovo.length() >= 1) {
+
+			if (arqNovo.exists() && arqNovo.length() >= 1) {
 				System.out.println("Arquivo criado com sucesso");
 			} else {
 				throw new IOException("Erro, arquivo criado está vazio");
